eb57bf6c1fc2c1eb16757484e6de14ddd3c2a892
Vincent Misson <git@misson.cloud>
2024-06-17 10:46:39 +0200

    add cicd demo config

---
+++ demo/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.74.0"
+    }
+  }
+  backend "azurerm" {
+    subscription_id      = "bfa339d4-ee2f-4040-810b-8ce1c3fb4877"
+    resource_group_name  = "tfstate"
+    storage_account_name = "cloud63tfstate"
+    container_name       = "tfstate"
+    key                  = "azurerm-azure-resources-demo.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
---
+++ demo/variables.tf
@@ -0,0 +1,98 @@
+variable "location" {
+  type    = string
+  default = "northeurope"
+}
+
+variable "firewall" {
+  type    = bool
+  default = true
+}
+
+variable "gateway" {
+  type    = bool
+  default = false
+}
+
+variable "bastion" {
+  type    = bool
+  default = true
+}
+
+variable "address_space_hub" {
+  type    = list(string)
+  default = ["10.100.0.0/24"]
+}
+
+variable "spoke_dns" {
+  type    = bool
+  default = true
+}
+
+variable "address_space_spoke_dns" {
+  type    = list(string)
+  default = ["10.100.1.0/24"]
+}
+
+variable "spoke_dmz" {
+  type    = bool
+  default = true
+}
+
+variable "address_space_spoke_dmz" {
+  type    = list(string)
+  default = ["10.100.2.0/24"]
+}
+
+variable "private_monitoring" {
+  type    = bool
+  default = false
+}
+
+variable "address_space_spoke_private_monitoring" {
+  type    = list(string)
+  default = []
+}
+
+variable "connection_monitor" {
+  type    = bool
+  default = false
+}
+
+variable "update_management" {
+  type    = bool
+  default = false
+}
+
+variable "network_security_group" {
+  type    = bool
+  default = false
+}
+
+variable "backup" {
+  type    = bool
+  default = false
+}
+
+variable "address_space_spokes" {
+  type = list(object({
+    workload        = string
+    environment     = string
+    instance        = string
+    address_space   = list(string)
+    virtual_machine = optional(bool, true)
+  }))
+  default = [
+    {
+      workload      = "app1"
+      environment   = "dev"
+      instance      = "001"
+      address_space = ["10.100.10.0/24"]
+    },
+    {
+      workload      = "app1"
+      environment   = "prd"
+      instance      = "001"
+      address_space = ["10.100.110.0/24"]
+    }
+  ]
+}
